7d6536258af613354568ffa5b0e593987fb970de
Zach Osman <zosman@gmu.edu>
2021-02-01 16:25:38 -0500

    init

---
+++ template-scenes/src/main.ts
@@ -0,0 +1,31 @@
+// import "./components/phaser.js";
+import * as Phaser from "phaser";
+import {Boot} from "./components/Boot";
+import {Game} from "./components/Game";
+import {MainMenu} from "./components/MainMenu";
+import {Preloader} from "./components/Preloader";
+
+
+// An object for shared (global) variables, so that them main menu can show
+// the high score if you want.
+let shared = {};
+
+// For a simpler example with multiple scenes:
+// https://phaser.io/examples/v3/view/scenes/changing-scene-es6
+
+// For a way to pass data between scenes without global variables:
+// https://phaser.io/examples/v3/view/scenes/passing-data-to-a-scene
+
+// You can have two scenes active at once, which can help separate a UI layer
+// from other things on the screen:
+// https://phaser.io/examples/v3/view/scenes/ui-scene-es6
+
+const game = new Phaser.Game({
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 800,
+    height: 600,
+    // The game starts the first scene in the scene array.
+    scene: [ Boot, Preloader, MainMenu, Game ],
+    physics: { default: 'arcade' },
+    });
