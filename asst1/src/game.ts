04dcdb17d9b9859d566acdca71cdc64ae97c6daf
Zach Osman <zosman@gmu.edu>
2021-02-09 01:33:12 -0500

    more functionality

--- asst1/src/game.ts
+++ asst1/src/game.ts
@@ -13,21 +13,40 @@ import * as Phaser from "phaser";
 
 // The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
 
+
+//examples from 
+//http://phaser.io/examples/v3/view/input/dragging/drag-time-threshold
 class MyScene extends Phaser.Scene {
     radius: number
+    greenfill: number;
+    redfill: number;
     
     constructor() {
         super(null);
         
         this.radius = 5000;
+        this.greenfill = 0;
+        this.redfill = 0;
     }
     
     preload() {
         // Load an image and call it 'logo'.
         this.load.image( 'map', 'assets/map.png' );
         this.load.image( 'greenbottle', "assets/greenbottle.png");
+        this.load.image( 'redbottle', "assets/redbottle.png")
     }
     
+    spawnGreen(){
+        const greenbottle = this.add.sprite(70, 400, 'greenbottle').setInteractive();
+        this.input.setDraggable(greenbottle);
+        greenbottle.name = "green"
+    }
+    spawnRed(){
+
+        const redbottle = this.add.sprite(800, 400, 'redbottle').setInteractive();
+        this.input.setDraggable(redbottle);
+        redbottle.name = "red"
+    }
     create() {
         
         const map = this.add.image(0, 0, 'map').setOrigin(0).setScale(.5);
@@ -37,9 +56,45 @@ class MyScene extends Phaser.Scene {
 
         this.lights.addLight(470, 500, this.radius).setColor(0xffffff).setIntensity(5);
 
+        this.spawnGreen();
+        this.spawnRed();
+
+
+        this.input.on('drag', function (pointer: any, gameObject: { x: any; y: any; }, dragX: any, dragY: any) {
+
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+    
+        });
+        // addGreen(){
+        //     this.greenfill +=1;
+        // }
+        const greenDrop = () => {
+            this.greenfill += 1
+            this.spawnGreen()
+        }
+        const redDrop = () => {
+            this.redfill += 1
+            this.spawnRed()
+        }
+        this.input.on('dragend', function (pointer: any, gameObject: { x: number; y: number; name: string; destroy: () => void; }) {
+            console.warn(gameObject.x);
+            console.warn(gameObject.y);
+            if(gameObject.x > 350 && gameObject.x < 500
+                && gameObject.y > 400){
+                    console.warn(gameObject.name);
+
+                    gameObject.name == "green" ? greenDrop() : redDrop()
+                    gameObject.destroy();
+
+                }
+    
+        });
+
     }
     
     update() {
+
       
     }
 }
