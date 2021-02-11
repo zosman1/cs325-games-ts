a3420cdf6bb16ca4d60e18141d2192821294af4f
Zach Osman <zosman@gmu.edu>
2021-02-11 04:46:33 -0500

    Finished Escape the darkness

--- asst1/src/game.ts
+++ asst1/src/game.ts
@@ -16,19 +16,26 @@ import * as Phaser from "phaser";
 
 //examples from 
 //http://phaser.io/examples/v3/view/input/dragging/drag-time-threshold
+//https://blog.ourcade.co/posts/2020/phaser-3-object-reveal-flashlight-spotlight-magic-lens/
 class MyScene extends Phaser.Scene {
     radius: number
     greenfill: number;
     redfill: number;
     text: Phaser.GameObjects.Text;
+    cover: Phaser.GameObjects.Image;
+    light: Phaser.GameObjects.Arc;
+    renderTexture: Phaser.GameObjects.RenderTexture;
+    gameover: boolean;
+    textHelper: Phaser.GameObjects.Text;
     
     constructor() {
         super(null);
         
-        this.radius = 5000;
-        this.greenfill = 0;
-        this.redfill = 0;
+        this.radius = 100;
+        this.greenfill = 3;
+        this.redfill = 3;
         this.text = null;
+        this.gameover = false;
     }
     
     preload() {
@@ -50,14 +57,37 @@ class MyScene extends Phaser.Scene {
         redbottle.name = "red"
     }
     create() {
-        
-        const map = this.add.image(0, 0, 'map').setOrigin(0).setScale(.5);
-        this.lights.enable().setAmbientColor(0x111111);
+		const reveal = this.add.image(0, 0, 'map').setOrigin(0).setScale(.5);
+		this.cover = this.add.image(0, 0, 'map').setOrigin(0).setScale(.5);
+		this.cover.setTint(0x002c49)
 
-        map.setPipeline('Light2D');
+		const width = this.cover.width
+		const height = this.cover.height
 
-        this.lights.addLight(470, 500, this.radius).setColor(0xffffff).setIntensity(5);
+		const rt = this.make.renderTexture({
+			width,
+            height,
+            //@ts-ignore-next-line
+			add: false
+		})
 
+		const maskImage = this.make.image({
+			x: 0,
+			y: 0,
+			key: rt.texture.key,
+			add: false
+		})
+
+		this.cover.mask = new Phaser.Display.Masks.BitmapMask(this, maskImage)
+		this.cover.mask.invertAlpha = true
+
+		reveal.mask = new Phaser.Display.Masks.BitmapMask(this, maskImage)
+
+		// this.light = this.add.circle(0, 0, 30, 0x000000, 1)
+		// this.light.visible = false
+
+
+		this.renderTexture = rt 
         this.spawnGreen();
         this.spawnRed();
 
@@ -94,12 +124,90 @@ class MyScene extends Phaser.Scene {
         });
 
         this.text = this.add.text(0,0, "hello", { fontFamily: 'Georgia'});
+        this.textHelper = this.add.text(0,200, "The darkness is closing in, mix red and \ngreen chemicals to create.. light.", { fontFamily: 'Georgia', fontSize: "40px"});
 
+        
+        setTimeout(() => setInterval(() => this.update_game(), 2000), 3000);   
+            //  setTimeout(() => this.win_game(), 200);
+
+        setTimeout(() => this.win_game(), 120000);
+    }
+
+    win_game(){
+        this.gameover = true;
+        this.textHelper.text = "You won! You survived the darkness,\n and became one with the light.\n Live Long, and Prosper."
+    }
+    update_game(){
+        if(this.gameover){
+            // this.textHelper.text = "Game Over!";
+            return;
+        }
+        this.textHelper.text = ""
+
+        if(this.greenfill == 0 || this.redfill == 0){
+            this.radius = this.radius - 50
+        } else {
+            if(this.radius < 500) this.radius = this.radius + 50
+        }
+
+        // if(this.radius < 500)
+        this.greenfill = this.greenfill - Math.floor(Math.random() * 3)
+        this.redfill = this.redfill - Math.floor(Math.random() * 3)
+        if(this.greenfill < 0){
+            this.greenfill = 0
+        }
+        if(this.redfill < 0){
+            this.redfill = 0
+        }
+
+
+   
+        
+
+        if(this.radius < 70){
+            this.gameover = true;
+            this.textHelper.text = "The darkness took you,\n may you rest, in peace."
+            // alert("You lost! Reload to try again")
+        }
+
+        if(this.gameover){
+            this.text.text = "Game Over!";
+        }
+
+        // alert(this.radius)
+        // if
+        // alert(this.greenfill)
     }
     
     update() {
+        if(this.gameover){
+            return
+        }
+        const greenBar = this.add.graphics();
+        const greenBox = this.add.graphics();
+        greenBox.fillStyle(0x222222, 0.6);
+        greenBox.fillRect(240, 600, 50, -200);
 
-      this.text.text = "Greenfill: " + this.greenfill + "  Redfill: " + this.redfill;
+        greenBar.fillStyle(0x00ff00, 1);
+        greenBar.fillRect(240, 600, 50, -200*this.greenfill/10)
+
+        const redBar = this.add.graphics();
+        const redBox = this.add.graphics();
+        redBox.fillStyle(0x222222, 0.6);
+        redBox.fillRect(630, 600, 50, -200);
+
+        redBar.fillStyle(0xff0000, 1);
+        redBar.fillRect(630, 600, 50, -200*this.redfill/10)
+
+        const lcircle= this.add.circle(0, 0, this.radius, 0x000000, 1)
+        lcircle.visible = false;
+        this.renderTexture.clear()
+        this.renderTexture.draw(lcircle, 1330, 1100)
+        this.text.text = "Greenfill: " + this.greenfill + "  Redfill: " + this.redfill;
+        if(this.gameover){
+            this.text.text = "Game Over!";
+        }
+
     }
 }
 
